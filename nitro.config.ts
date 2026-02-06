b7514ed898a7eb1c78b631946d08982a1c251e30
it-ipac <it@ipac.ae>
2026-02-06 18:38:18 +0400

    chore: test

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,11 +2,4 @@ import { defineNitroConfig } from 'nitro/config'
 
 export default defineNitroConfig({
   preset: 'vercel',
-  // Disable node modules tracing to avoid Windows nf3 issues
-  externals: {
-    noTrace: true,
-  },
-  experimental: {
-    wasm: false,
-  },
 })
